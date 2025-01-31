8dcca64706892af200bea4d4f3e2d47cda4249d6
linlin <sdsantoscarla@gmail.com>
2025-01-31 16:27:00 -0300

    projeto finalizado

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,17 +1,19 @@
 const mode = document.getElementById('mode_icon'); //pega o ícone de modo
-const form = document.getElementById('login_form');
 
 mode.addEventListener('click', () => { //adicionar evento de click
-    const form = document.getElementById('login_form'); //pga o formulário
+ //pega o formulário
+
+ const form = document.getElementById('login_form');
 
     if(mode.classList.contains('fa-moon')) { //verifica se contem o ícone da lua
         mode.classList.remove('fa-moon'); //remove o ícone da lua
         mode.classList.add('fa-sun'); //adiciona o ícone de sol
-        form.classList.add('dark')
-    }else{
-        mode.classList.add('fa-moon');
+
+        form.classList.add('dark');
+        return;
+    }
+    
+    mode.classList.add('fa-moon');
     mode.classList.remove('fa-sun');
     form.classList.remove('dark'); //remove a classe 'dark'
-    }
 });
-
